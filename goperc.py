7814e5ed0a055fdce3e64e9b34f9b5000f8b9f92
Prateek Dubey <prateeklndubey@gmail.com>
2020-11-04 18:40:15 +0530

    Added phase division facility!
    Now you can divide your goals into 4 phases, 10%, 30%, 60%, and the obvious 100% of the goal.

--- goperc.py
+++ goperc.py
@@ -37,21 +37,44 @@ def getDifference(dt1, dt2):
 def main():
 	dt1 = Date(26, 10, 2020)
 	days = 1500
+	p1 = int(days/10)
+	p2 = int(days/3.3333333333333333333333333)
+	p3 = int(days/1.6666666666666666666666667)
 	today = date.today() 
 	dt2 = Date(today.day, today.month, today.year)
 	diff = getDifference(dt1, dt2)
+	diff_p1 = diff_p2 = diff_p3 = diff
 	perc = diff*100/days
 	rem = days - diff
-	for i in tqdm(range(diff), total = days, desc ="Progress Bar", ncols=100):
-		if i == 0:
-			system('cls')
+	if diff_p1 >= p1:
+		diff_p1 = int(days/10)
+	if diff_p2 >= p2:
+		diff_p2 = int(days/3.3333333333333333333333333)
+	if diff_p3 >= p3:
+		diff_p3 = int(days/1.6666666666666666666666667)
+	x = 101
+	if diff >= 1000:
+		x = 102
+	for i in tqdm(range(diff_p1), total = p1, desc ="Phase 1"):
+		pass
+	for i in tqdm(range(diff_p2), total = p2, desc ="Phase 2"):
+		pass
+	for i in tqdm(range(diff_p3), total = p3, desc ="Phase 3"):
+		pass
+	for i in tqdm(range(diff), total = days, desc ="Course "):
 		pass
 	print(round(perc,2),"% of the goal is achieved, and a streak of ",diff," days or ",round(diff/7)," weeks or ",round(diff/30.4167)," months has been reached.\nYou have to maintain it for ",rem," days or ",round(rem/7)," weeks or ",round(rem/30.4167)," months more. \nYou must achieve the ",days," days or ",round(days/7)," weeks or ", round(days/30.4167)," months mark.",sep="")
+	if diff >= p1:
+		print("You have successfully completed the first phase. Keep Hustling!!")
+	if diff >= p2:
+		print("You have successfully completed the Second phase. Keep Hustling!!")
+	if diff >= p3:
+		print("You have successfully completed the Third phase. Keep Hustling!!")
 	if diff == days:
-		print("You have done it mere cheete!!")
+		print("You have done it mere cheeete!!")
 	if diff > days:
 		system('cls')
-		print("Attention don't enjoy, continue the streak!!\nYou have to achive a lot more.")
+		print("Attention!! don't enjoy, continue the streak!!\nYou have to achive a lot more.")
 	k=input("press enter to exit...")
 
 if __name__=="__main__":
